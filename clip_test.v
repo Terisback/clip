69c9cdef57e7f595435f59d3fbc4bef3bc3fbb66
Anton Zavodchikov <terisbackno@gmail.com>
2021-08-17 04:06:04 +0500

    Add test for append

--- clip_test.v
+++ clip_test.v
@@ -56,3 +56,10 @@ fn test_app_constructor() {
 		footer: 'Some cli app usage examples'
 	})
 }
+
+fn test_append() {
+	a := [1, 2, 3]
+	b := [4, 5]
+	c := append(a, ...b)
+	assert [1, 2, 3, 4, 5] == c
+}
